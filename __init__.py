249e4e8b74d04bd757bf1b45b870db07397686e3
Luigi Pirelli <luipir@gmail.com>
2014-09-05 13:27:28 +0200

    aligned init to current plugin parameter setting using metadata.txt

--- __init__.py
+++ __init__.py
@@ -22,30 +22,6 @@ __copyright__ = '(C) 2012, Giuseppe Sucameli'
 # This will get replaced with a git SHA1 when you do a git archive
 __revision__ = '$Format:%H$'
 
-def name():
-    return "Processing LWGEOM provider"
-
-def description():
-    return "Expose liblwgeom functions to Processing."
-
-def author():
-    return "Giuseppe Sucameli (Faunalia)"
-
-def authorName():  # for compatibility only
-    return author()
-
-def email():
-    return "sucameli@faunalia.it"
-
-def version():
-    return "0.4444"
-
-def icon():
-    return "icon.png"
-
-def qgisMinimumVersion():
-    return "2.0"
-
 def classFactory(iface):
     from processinglwgeomprovider.ProcessingLwgeomProviderPlugin import ProcessingLwgeomProviderPlugin
     return ProcessingLwgeomProviderPlugin()
